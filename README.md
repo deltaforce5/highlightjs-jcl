7733a017acc20ac2909818c163a40cf1858a6b79
deltaforce5 <djdeltaforce5@gmail.com>
2022-11-27 21:05:18 +0100

    implemented keywords and statements

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # highlightjs-jcl
 highlight.js grammar for Job Control Language (JCL)
 
+## Status
+draft
+
 ## build Highlight.js
 
 1. clone <code>highlight.js</code> repository (follow highlight.js instructions)
 2. clone this repository into a different directory
-3. create a symbolic link of the highlightjs-jcl directory into the highlight.js extra directory: <code> ln -s ./highlightjs-jcl/ ./highlight.js/extra/highlightjs.jcl</code>
+3. create a symbolic link of the highlightjs-jcl directory into the highlight.js extra directory: <code>ln -sr ./highlightjs-jcl/ ./highlight.js/extra/</code>
 4. enter into highlight.js directory and run: <code>npm install</code>
 5. build your highlight.js
    1. jcl only language: <code>node tools/build -n jcl</code>
--- src/languages/jcl.js
+++ src/languages/jcl.js
@@ -6,367 +6,159 @@ Website: https://en.wikipedia.org/wiki/Job_Control_Language
 */
 
 export default function(hljs) {
-    const KEYWORDS = [ 'ALIAS',
-        'ALTER',
-        'ALTERNATEINDEX',
-        'ALTSEQ',
-        'AVGREC',
-        'BLDINDEX',
-        'BLKSIZE',
-        'BURST',
-        'CDAY',
-        'CDD',
-        'CDDD',
-        'CDDMMYY',
-        'CHH',
-        'CHHMM',
-        'CHHMMSSX',
-        'CLASS',
-        'CLUSTER',
-        'CMM',
-        'CMMYY',
-        'CNTL',
-        'CNVTCAT',
-        'COMMAND',
-        'CONNECT',
-        'COPY',
-        'COUNT',
-        'CWW',
-        'CWWD',
-        'CYMD',
-        'CYY',
-        'CYYDDD',
-        'CYYMM',
-        'CYYMMDD',
-        'CYYYY',
-        'CYYYYMM',
-        'DCB',
-        'DEBUG',
-        'DEFAULTS',
-        'DEFINE',
-        'DELETE',
-        'DISCONNECT',
-        'DISP',
-        'DISPLAY',
-        'DSN',
-        'DSORG',
-        'DUMMY',
-        'DYNAM',
-        'ELSE',
-        'END',
-        'ENDCNTL',
-        'ENDIF',
-        'EXAMINE',
-        'EXEC',
-        'EXPORT',
-        'EXPORTRA',
-        'GDG',
-        'GENERATIONDATAGROUP',
-        'IF',
-        'IMPORT',
-        'IMPORTRA',
-        'INCLUDE',
-        'INREC',
-        'JCLLIB',
-        'JOB',
-        'KEYLEN',
-        'LABEL',
-        'LIKE',
-        'LISTCAT',
-        'LISTCRA',
-        'LRECL',
-        'MERGE',
-        'MODE',
-        'MODS',
-        'MSGCLASS',
-        'MSGLEVEL',
-        'NONVSAM',
-        'NOTIFY',
-        'OCCUR',
-        'OMIT',
-        'OPTION',
-        'OUTFIL',
-        'OUTPUT',
-        'OUTREC',
-        'PAGESPACE',
-        'PATH',
-        'PEND',
-        'PGM',
-        'PRINT',
-        'PROC',
-        'RANGE',
-        'RECFM',
-        'RECORD',
-        'REGION',
-        'REPRO',
-        'RESETCAT',
-        'SELECT',
-        'SET',
-        'SORT',
-        'SPACE',
-        'STATS',
-        'STORCLAS',
-        'SUM',
-        'THEN',
-        'UNIQUE',
-        'UNIT',
-        'USERCATALOG',
-        'VERIFY',
-        'VOLUME',
-        'XMIT'
-    ];
-    const LITERALS = [
-        'OLD',
-        'NEW',
-        'MOD',
-        'KEEP',
-        'CATLG',
-        'DELETE',
-        'FB',
-        'CYL',
-        'TRK'
-    ];
-    const DD_PARMS = [
-        '*',
-        'ACCODE',
-        'AMP',
-        'AVGREC',
-        'BLKSIZE',
-        'BLKSZLIM',
-        'BURST',
-        'CCSID',
-        'CHARS',
-        'CHKPT',
-        'CNTL',
-        'COPIES',
-        'DATA',
-        'DATACLAS',
-        'DCB',
-        'DDNAME',
-        'DEST',
-        'DISP',
-        'DLM',
-        'DSID',
-        'DSKEYLBL',
-        'DSNAME',
-        'DSNTYPE',
-        'DUMMY',
-        'DYNAM',
-        'EATTR',
-        'EXPDT',
-        'FCB',
-        'FILEDATA',
-        'FLASH',
-        'FREE',
-        'FREEVOL',
-        'GDGORDER',
-        'HOLD',
-        'KEYLABL1',
-        'KEYLABL2',
-        'KEYENCD1',
-        'KEYENCD2',
-        'KEYLEN',
-        'KEYOFF',
-        'LABEL',
-        'LGSTREAM',
-        'LIKE',
-        'LRECL',
-        'MAXGENS',
-        'MGMTCLAS',
-        'MODIFY',
-        'OUTLIM',
-        'OUTPUT',
-        'PATH',
-        'PATHDISP',
-        'PATHMODE',
-        'PATHOPTS',
-        'PROTECT',
-        'RECFM',
-        'RECORG',
-        'REFDD',
-        'RETPD',
-        'RLS',
-        'ROACCESS',
-        'SECMODEL',
-        'SEGMENT',
-        'SPACE',
-        'SPIN',
-        'STORCLAS',
-        'SUBSYS',
-        'SYMBOLS',
-        'SYMLIST',
-        'SYSOUT',
-        'TERM',
-        'UCS',
-        'UNIT',
-        'VOLUME'
-    ]
-    const OPC_KEYWORDS = [
-        'SETVAR',
-        'SCAN'
-    ];
+    const CARD_NAME_RE = '[A-Z0-9\d\$\#\@ ]{1,8}?';
+    const KEYWORDS = 'CNTL COMMAND DD ELSE ENDCNTL ENDIF EXEC IF JCLLIB JOB OUTPUT PEND PROC SET THEN XMIT ';
+    const DD_KEYWORDS = '* ACCODE AMP AVGREC BLKSIZE BLKSZLIM BURST CCSID CHARS CHKPT CNTL COPIES DATA DATACLAS DCB ' +
+                        'DDNAME DEST DISP DLM DSID DSKEYLBL DSNAME DSNTYPE DUMMY DYNAM EATTR EXPDT FCB FILEDATA FLASH '+
+                        'FREE FREEVOL GDGORDER HOLD KEYLABL1 KEYLABL2 KEYENCD1 KEYENCD2 KEYLEN KEYOFF LABEL LGSTREAM ' +
+                        'LIKE LRECL MAXGENS MGMTCLAS MODIFY OUTLIM OUTPUT PATH PATHDISP PATHMODE PATHOPTS PROTECT ' +
+                        'RECFM RECORG REFDD RETPD RLS ROACCESS SECMODEL SEGMENT SPACE SPIN STORCLAS SUBSYS SYMBOLS ' +
+                        'SYMLIST SYSOUT TERM UCS UNIT VOLUME ';
+    const DD_LITERALS = 'CATLG CYL DELETE FB KEEP NEW OLD PASS RLSE SHR SL TRK VB ';
+    const JOB_KEYWORDS = 'ADDRSPC BYTES CARDS CCSID CLASS COND DSENQSHR GROUP JESLOG JOBRC LINES MEMLIMIT MSGCLASS ' +
+                        'MSGLEVEL NOTIFY PAGES PASSWORD PERFORM PRTY RD REGION RESTART SECLABEL SCHENV SYSAFF SYSTEM ' +
+                        'TIME TYPRUN UJOBCORR USER ' ;
+    const OPC_KEYWORDS = 'BEGIN END SCAN SETFORM SETVAR CDATE CTIME OCDATE OCFRSTC OCFRSTW OCFRSTWY OCLASTC OCLASTW ' +
+                        'OCLASTWY OCTIME OPIADATE OPIATIME OPLSDATE OPLSTIME OPC ';
+    const OPC_LITERALS = 'OADID OADOWNER OAUGROUP OCALID ODAY ODD ODDD ODMY1 ODMY2 OETCRIT OETEVNM OETGGEN OETGROOT ' +
+                        'OETJNUM OETJOBN OETTYPE OFREEDAY OHH OHHMM OMM OMMYY OWW OWWD OWWLAST OWWMONTH OYMD OYM ' +
+                        'OYMD1 OYMD2 OYMD3 OYY OYYDDD OYYMM OYYYY OJOBNAME OLDAY OLDD OLHH OLHHMM OLMD OLMM OLWK ' +
+                        'OLYMD OLYYDDD OOPNO OWSID OXJOBNAM CDAY CDD CDDD CDDMMYY CFREEDAY CHH CHHMM CHHMMSS CHHMMSSX ' +
+                        'CMM CMMYY CWW CWWD CYMD CYY CYYDDD CYYMM CYYMMDD CYYYY CYYYYMM SETUP SUBMIT ';
+    const NUMBERS_QUANTIFIERS = 'M K WD CD WK MO YR HH MM SS';
+    const JES2_CTRL_KEYWORDS = 'JOBPARM MESSAGE NETACCT NOTIFY OUTPUT PRIORITY ROUTE SETUP SIGNOFF SIGNON XEQ XMIT ';
+
     const STRINGS = {
         className: 'string',
         begin: '\'',
         end: '\'',
         illegal: '\\n'
     };
-    const VARIABLES = {
-        className: 'variables',
-        begin: '&[0-9a-zA-Z]',
-        end: '.',
-        relevance: 0
-    }
-    const PARAMS = {
-        className: 'params',
-        keywords: DD_PARMS,
-        begin: '[0-9a-zA-Z.]*=&?[0-9a-zA-Z.]*',
-        end: '[,]?',
-        contains: [
-            {
-                className: 'subparameter',
-                begin: /\(/,
-                end: /\)/,
-                contains: [ "self" ]
-            }
-        ],
-        relevance: 0
-    };
-    const CONT_STMT = {
-        className: 'continuation',
-        begin: '\/\/[\ ]+',
-        end: '',
-        contains: [
-            PARAMS
-        ],
-        relevance: 0
-    }
 
-    // FIX use
     const NUMBERS = {
         className: 'number',
-        begin: '\\b(0b[01\']+)',
-        end: ''
+        match: '\\b[\\d\']+('.concat(NUMBERS_QUANTIFIERS.replace('\ ', '|') + ')?\\b'),
+        relevance: 0
     };
 
-    // FIX multiline stmt
-    const JOB_STMT = {
-        className: 'JOB statement',
-        begin: '\/\/[a-zA-Z0-9\$\#\@][a-zA-Z0-9\$\#\@\s]{1,7}? JOB',
-        end: '',
-        contains: [
-            /*JCL_STEP*/
-            PARAMS,
-            CONT_STMT
+    const PUNCTUATION = {
+        className: 'punctuation',
+        keywords: '( ) = & , ^ ! < >',
+        relevance: 0
+    };
+
+    const VARIABLES = {
+        className: 'variables',
+        match: /&[&]?[A-Z0-9]+/,
+        relevance: 0
+    }
+
+    const COMMENT_INLINE = {
+        className: 'comment',
+        begin: /--/,
+        end: /$/,
+        relevance: 0
+    };
+
+    const DD_INSTREAM = {
+        className: 'meta',
+        begin: /\s*\*[.\n]*/,
+        end: /\/(.*?)\n/,
+        relevance: 0
+    }
+
+    const PARAMS = {
+        className: 'params',
+        keywords: {
+            keyword: DD_KEYWORDS + JOB_KEYWORDS + OPC_KEYWORDS,
+            literal: DD_LITERALS + OPC_LITERALS,
+            $pattern: /\w+/
+        },
+
+        begin: [
+            /[A-Z\d\$\#\@]*/,
+            /=/
         ],
-        relevance: 10
+        end: /[,]?/,
+        beginScope: {
+            1: 'keyword',
+            2: 'punctuation'
+        },
+        contains: [
+            'self',
+            VARIABLES,
+            NUMBERS,
+            STRINGS,
+            {
+                className: 'values',
+                keywords: {
+                    keyword: DD_KEYWORDS + JOB_KEYWORDS + OPC_KEYWORDS,
+                    literal: DD_LITERALS + OPC_LITERALS
+                },
+                variants: [
+                    { begin: /[\w\d.\*]+/, end: '' },
+                    { begin: /\(/, end: /\)/ }
+                ],
+                contains: [ 'self', VARIABLES, NUMBERS, STRINGS ]
+            },
+            STRINGS
+        ],
+        relevance: 0
     };
 
-
-    // FIX Multiline DD
-    const DD_CARD = {
-        className: 'DD card',
-        begin: '\/\/[A-Z0-9\$\#\@][A-Z0-9\ \$\#\@]{1,7}? DD ',
+    const JES2_CTRL_STMT = {
+        className: 'JES2 control statement',
+        keywords: JES2_CTRL_KEYWORDS,
+        begin: /^\/\*[A-Z]+\b/,
         end: /$/,
         contains: [
             PARAMS,
-            CONT_STMT
-        ],
-        relevance: 0
-    };
-    const JCL_STEP = {
-        className: 'JOB step',
-        begin: '\/\/[A-Z0-9\$\#\@][A-Z0-9\$\#\@\ ]{1,7}? ',
-        end: '/$/',
-        keywords: { keyword: 'EXEC PROC' },
-        contains: [
-            DD_CARD,
-            { endsWithParent: true }
+            COMMENT_INLINE
         ]
     };
 
-    // FIX Unrecognized directive
-    const OPC_DIRECTIVE = {
+   const OPC_DIRECTIVE = {
         className: 'OPC directive',
-        begin: '\/\/\*%OPC',
-        end: '',
-        keywords: { OPC_KEYWORDS },
+        begin: /\/\/\*(%|>)OPC/,
+        end: /$/,
+        keywords: OPC_KEYWORDS,
         contains: [
             PARAMS,
-            {
-                className: 'subparameter',
-                begin: /\(/,
-                end: /\)/,
-                contains: [ "self" ]
-            }
-        ],
-        relevance: 1
+            COMMENT_INLINE
+        ]
     };
 
-    // FIX Unrecognized directive
-    const OPC_PDIRECTIVE = {
-        className: 'OPC processed directive',
-        begin: '\/\/\>\%OPC',
-        relevance: 0
+    const JCLCARD = {
+        className: 'jclcard',
+        begin: [
+            /^\/\//,
+            CARD_NAME_RE,
+            /\s{1}/,
+            /(.*?)/
+        ],
+        beginScope: {
+            2: 'symbol'
+        },
+        end: /$/,
+        keywords: {
+            keyword: KEYWORDS,
+            literal: DD_LITERALS
+        },
+        contains: [
+            PARAMS,
+            DD_INSTREAM
+        ]
     }
-
 
     return {
         name: 'JCL',
         case_insensitive: false,
-        keywords: {
-            keyword: KEYWORDS,
-            literal: LITERALS
-        },
         contains: [
-            JOB_STMT,
             OPC_DIRECTIVE,
-            OPC_PDIRECTIVE,
-            DD_CARD,
-            VARIABLES,
-            PARAMS,
-            hljs.COMMENT('\\/\\/\\*[^%+](.*)', '', { relevance: 0 }),
+            hljs.COMMENT( /^\/\/\*[^%>]/, /$/ ),
+            JES2_CTRL_STMT,
+            JCLCARD,
             STRINGS
         ]
- /*       contains: [
-            {
-                className: 'JOB statement',
-                begin: '\/\/[a-zA-Z0-0\$\#\@]{1,8}? JOB',
-                relevance: 10
-            },
-            {
-                className: 'OPC directive',
-                begin: '\/\/\*\%OPC',
-                excludeEnd: true,
-                relevance: 0
-            },
-            {
-                className: 'OPC processed directive',
-                begin: '\/\/\>\%OPC',
-                excludeEnd: true,
-                relevance: 0
-            },
-            {
-                className: 'variables',
-                begin: '&[0-9a-zA-Z]',
-                end: '.',
-                relevance: 0
-            },
-            {
-                className: 'param',
-                begin: '=&?[0-9a-zA-Z.]*',
-                end: '',
-                relevance: 0
-            },
-            hljs.COMMENT('\\/\\/\\*[^%+](.*?)', '\\n', { relevance: 0 })
-            // The following regular expressions solves three purposes
-            // - Identify Temp Variables in JCL (e.g. &&TEMP)
-            // - Symbolic variables in JCL (e.g. &SYSUID)
-            // - TWS OPC Variables (e.g. %OPC)
-            // Thanks to Simon for pointing me to this
-            //     0 => '&amp;&amp;[a-zA-Z]{1,8}[0-9]{0,}',
-            // 1 => '&amp;[a-zA-Z]{1,8}[0-9]{0,}',
-            // 2 => '&amp;|\?|%[a-zA-Z]{1,8}[0-9]{0,}'
-        ],*/
     }
 }
